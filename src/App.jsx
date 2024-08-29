b2a22b7107de60a30d711fedd00f3dc32c2cf6c0
komarnetwork <komar.network@gmail.com>
2024-08-29 10:28:15 +0700

    Component & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+const Button = (props) => {
+  const { variant = 'bg-black', children } = props;
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type='submit'
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className='flex justify-center bg-blue-600 items-center h-screen'>
-      <h1>Hello World</h1>
+      <div className='flex gap-x-3'>
+        <Button variant='bg-red-700'>Login</Button>
+        <Button variant='bg-green-700'>Logout</Button>
+        <Button></Button>
+      </div>
     </div>
   );
 }
